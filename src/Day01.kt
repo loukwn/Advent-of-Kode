d817ed5e9e4bdf88de090f3d150a6225502b8cb9
loukwn <loukwn@gmail.com>
2023-12-01 11:11:54 +0000

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,52 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            val (first, last) = line.first { it.isDigit() }.toString() to line.last { it.isDigit() }.toString()
+            "$first$last".toInt()
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val stringDigits = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+        return input.sumOf { line ->
+
+            val firstDigitIndex = line.indexOfFirst { it.isDigit() }
+            val lastDigitIndex = line.indexOfLast { it.isDigit() }
+
+            val stringDigitMatches = arrayListOf<Pair<String, Int>>()
+
+            stringDigits.forEach {
+                val firstOccurenceIndex = line.indexOf(it)
+                val lastOccurenceIndex = line.lastIndexOf(it)
+
+                stringDigitMatches.add(it to firstOccurenceIndex)
+                if (firstOccurenceIndex != lastOccurenceIndex) {
+                    stringDigitMatches.add(it to lastOccurenceIndex)
+                }
+            }
+
+            val sortedStringDigits = stringDigitMatches.filterNot { it.second == -1 }.sortedBy { it.second }
+            val firstStringDigitInLine = sortedStringDigits.firstOrNull()
+            val lastStringDigitInLine = sortedStringDigits.lastOrNull()
+
+            val firstDigit = if (firstStringDigitInLine == null || (firstDigitIndex < firstStringDigitInLine.second && firstDigitIndex != -1)) {
+                line[firstDigitIndex]
+            } else {
+                stringDigits.indexOf(firstStringDigitInLine.first) + 1
+            }
+
+            val lastDigit = if (lastStringDigitInLine == null || (lastDigitIndex > lastStringDigitInLine.second)) {
+                line[lastDigitIndex]
+            } else {
+                stringDigits.indexOf(lastStringDigitInLine.first) + 1
+            }
+
+            "$firstDigit$lastDigit".toInt()
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInput("inputs/Day01")
     part1(input).println()
     part2(input).println()
 }
