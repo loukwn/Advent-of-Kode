1cb4d5bf6ca06a049dc19c1ca4284dc88f7f6d90
loukwn <loukwn@gmail.com>
2023-12-06 09:28:13 +0000

    Day 6

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,12 +1,28 @@
 import kotlin.time.measureTimedValue
 
 fun main() {
-    fun part1(input: List<String>): Long {
-        return 0
+    fun part1(input: List<String>): Int {
+        val times = input[0].split(" ").drop(1).filter { it.isNotEmpty() }.map { it.trim().toInt() }
+        val distances = input[1].split(" ").drop(1).filter { it.isNotEmpty() }.map { it.trim().toInt() }
+
+        return times.zip(distances).map { (time, recordDistance) ->
+            (0..time).count { heldTime ->
+                val distanceTraveled = (time - heldTime) * heldTime
+
+                recordDistance < distanceTraveled
+            }
+        }.reduce(Int::times)
     }
 
-    fun part2(input: List<String>): Long {
-        return 0
+    fun part2(input: List<String>): Int {
+        val time = input[0].replace(" ", "").split(":")[1].toLong()
+        val distance = input[1].replace(" ", "").split(":")[1].toLong()
+
+        return (0..time).count { heldTime ->
+            val distanceTraveled = (time - heldTime) * heldTime
+
+            distance < distanceTraveled
+        }
     }
 
     val input = readInput("inputs/Day06")
---
+++ src/Day07.kt
@@ -0,0 +1,16 @@
+import kotlin.time.measureTimedValue
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val input = readInput("inputs/Day07")
+
+    measureTimedValue { part1(input) }.println()
+    measureTimedValue { part2(input) }.println()
+}
