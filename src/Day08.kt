a80e26fa79d8fd9f49835acb992add13c149eacf
loukwn <loukwn@gmail.com>
2023-12-08 11:16:56 +0000

    Day 8

--- src/Day08.kt
+++ src/Day08.kt
@@ -2,11 +2,51 @@ import kotlin.time.measureTimedValue
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return 0
+        val navigation = input[0].map { if (it == 'L') 0 else 1 }
+        val map = input.drop(2).associate { line ->
+            val currentPosition = line.substringBefore("=").trim()
+            val positionsToNavigateFromCurrent = line.substringAfter("=").replace("[ ()]".toRegex(), "").split(",")
+
+            currentPosition to positionsToNavigateFromCurrent
+        }
+
+        var steps = 0
+        var currentPosition = "AAA"
+
+        do {
+            currentPosition = map[currentPosition]!![navigation[(steps % navigation.size).toInt()]]
+            steps++
+        } while (currentPosition != "ZZZ")
+
+        return steps
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long {
+        val navigation = input[0].map { if (it == 'L') 0 else 1 }
+        val map = input.drop(2).associate { line ->
+            val currentPosition = line.substringBefore("=").trim()
+            val positionsToNavigateFromCurrent = line.substringAfter("=").replace("[ ()]".toRegex(), "").split(",")
+
+            currentPosition to positionsToNavigateFromCurrent
+        }
+        var steps = 0
+        var currentPositions = map.keys.filter { it.endsWith('A') }
+        val foundEndAt = currentPositions.map { 0 }.toMutableList()
+
+        do {
+            currentPositions = currentPositions.map {
+                map[it]!![navigation[(steps % navigation.size)]]
+            }
+
+            steps++
+            currentPositions.forEachIndexed { index, s ->
+                if (s.endsWith("Z")) {
+                    foundEndAt[index] = steps
+                }
+            }
+        } while (foundEndAt.any { it == 0 })
+
+        return lcm(foundEndAt)
     }
 
     val input = readInput("inputs/Day08")
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,23 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun gcd(a: Long, b: Long): Long {
+    var reducedB = b
+    var newA = a
+    while (reducedB > 0) {
+        val temp = reducedB
+        reducedB = a % reducedB
+        newA = temp
+    }
+
+    return newA
+}
+
+fun lcm(a: Long, b: Long): Long {
+    return a * (b / gcd(a, b))
+}
+
+fun lcm(nums: List<Int>): Long {
+    return nums.fold(nums[0].toLong()) { acc, l -> lcm(acc, l.toLong()) }
+}
