1406131d1077202fae206ae0c04c31f56481069b
loukwn <loukwn@gmail.com>
2023-12-09 11:04:37 +0000

    Day 10

--- src/Day09.kt
+++ src/Day09.kt
@@ -2,11 +2,27 @@ import kotlin.time.measureTimedValue
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return 0
+        return input.sumOf { line ->
+            val histories = arrayListOf(line.split(" ").map { it.toInt() })
+            while (histories.last().toSet().size != 1) {
+                histories.add(histories.last().zipWithNext { a, b -> b - a })
+            }
+            histories.reversed().drop(1).fold(histories.last().last()) { previousLast, history ->
+                history.last() + previousLast
+            }
+        }
     }
 
-    fun part2(input: List<String>): Long {
-        return 0
+    fun part2(input: List<String>): Int {
+        return input.sumOf { line ->
+            val histories = arrayListOf(line.split(" ").map { it.toInt() })
+            while (histories.last().toSet().size != 1) {
+                histories.add(histories.last().zipWithNext { a, b -> b - a })
+            }
+            histories.reversed().drop(1).fold(histories.last().first()) { previousLast, history ->
+                history.first() - previousLast
+            }
+        }
     }
 
     val input = readInput("inputs/Day09")
